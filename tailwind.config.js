554c22b815bcc416cac2b103ab000ad943d78f8a
asyaselinbaykal <s240307016@ankarabilim.edu.tr>
2025-12-15 12:35:02 +0300

    responsive tasarım yapıldı

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './**/*.py'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#D4AF37'
+      },
+      fontFamily: { display: ['Inter', 'sans-serif'] }
+    }
+  },
+  plugins: []
+};
